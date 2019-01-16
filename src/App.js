36ff671d0ab9bd3a93451515ba145ff33afa134c
Kjetil Jacobsen Villanger <kjetiljv@gmail.com>
2019-01-16 10:11:35 +0100

    Included Material-UI library
    https://material-ui.com/

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Starting point</h1>
+        <Button variant="contained" color="primary">
+          Button
+        </Button>
       </div>
     );
   }
