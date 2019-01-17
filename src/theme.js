f3fc178a900455dbea2961351b382d6a127b366f
Mads Grønbæk <mkg@7mountains.com>
2019-01-17 23:53:57 +0100

    try ta branch it

--- src/theme.js
+++ src/theme.js
@@ -15,7 +15,7 @@ export default createMuiTheme({
     
   background: {
       //"paper": "rgba(239, 158, 158, 1)",
-      default: "rgba(255, 0, 255, 1)"
+      default: "rgba(255, 0, 0, 1)"
 
   },
   primary: {
